705573d8b44d0781eecc208d234f7ecb1730de96
linmmill <35968916+linmmill@users.noreply.github.com>
2018-04-19 10:12:25 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,9 +3,47 @@ $(document).ready(function(){
     hoursOfOperation();
   });
 
-var phoneHours = ["2pm-7pm ET","7am-7pm ET","7am-7pm ET","7am-7pm ET","7am-7pm ET","7am-6pm ET","Closed"];
-var chatHours = ["Closed","8am-5pm ET","8am-5pm ET","8am-5pm ET","8am-5pm ET","8am-5pm ET","Closed"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const phoneHours = ["2pm-7pm ET","7am-7pm ET","7am-7pm ET","7am-7pm ET","7am-7pm ET","7am-6pm ET","Closed"];
+const chatHours = ["Closed","8am-5pm ET","8am-5pm ET","8am-5pm ET","8am-5pm ET","8am-5pm ET","Closed"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function hoursOfOperation() {
+
+    // // Assign current time to a variable
+    const now = new Date();
+    // const now = new Date("2018-04-17T17:00:00");
+    contactStatus = "unknown";
+    
+    console.log(now);
+    
+    if (
+        (now.getDay()>0 && now.getDay()<6) &&
+        (now.getUTCHours()>=12 && now.getUTCHours()<21)
+        ){
+        chatOpen();
+    } else {
+        chatClosed();
+    }
+    
+    console.log(chatStatus);
+    
+    if (
+        (now.getDay()>0 && now.getDay()<5) &&
+        (now.getUTCHours()>=11 && now.getUTCHours()<23)){
+        phoneOpen();
+    } else if(now.getDay()===0 && now.getUTCHours()>=18 && now.getUTCHours()<23){
+        phoneOpen();
+    } else if(
+        (now.getDay()===5) &&
+        (now.getUTCHours()>=11 && now.getUTCHours()<22)){
+            phoneOpen();
+    } else {
+            phoneClosed();
+    }
+    
+    console.log(phoneStatus);
+    
+    }
 
 function phoneClosed(){
     phoneStatus="CLOSED";
@@ -15,7 +53,7 @@ function phoneClosed(){
     var d = new Date();
     var nextDay = d.getDay()+1;
     var hoursOnDay = phoneHours[nextDay];
-    nextHours = "Tomorrow (" + days[nextDay] + "): " + hoursOnDay;
+    nextHours = "Tomorrow's Hours: " + days[nextDay] + " " + hoursOnDay;
     console.log(nextHours);
     document.getElementById("phoneStatus").innerHTML=phoneStatus;
     document.getElementById("phone-hours").innerHTML=nextHours;
@@ -27,7 +65,7 @@ function phoneOpen(){
     $( "#phoneStatus" ).addClass( "btn-status-open" );
     var d = new Date();
     var hoursOnDay = phoneHours[d.getDay()];
-    todayHours = "Today (" + days[d.getDay()] + "): " + hoursOnDay;
+    todayHours = "Today's Hours: " + days[d.getDay()] + " " + hoursOnDay;
     console.log(todayHours);
     document.getElementById("phoneStatus").innerHTML=phoneStatus;
     document.getElementById("phone-hours").innerHTML=todayHours;
@@ -41,7 +79,7 @@ function chatOpen(){
     $( "#chatStatus" ).addClass( "btn-status-open" );
     var d = new Date();
     var hoursOnDay = chatHours[d.getDay()];
-    todayHours = "Today (" + days[d.getDay()] + "): " + hoursOnDay;
+    todayHours = "Today's Hours: " + days[d.getDay()] + " " + hoursOnDay;
     console.log(todayHours);
     document.getElementById("chatStatus").innerHTML=chatStatus;
     document.getElementById("chat-hours").innerHTML=todayHours;
@@ -55,48 +93,12 @@ function chatClosed(){
     var d = new Date();
     var nextDay = d.getDay()+1;
     var hoursOnDay = chatHours[nextDay];
-    nextHours = "Tomorrow (" + days[nextDay] + "): " + hoursOnDay;
+    nextHours = "Tomorrow's Hours: " + days[nextDay] + " " + hoursOnDay;
     console.log(nextHours);
     document.getElementById("chatStatus").innerHTML=chatStatus;
     document.getElementById("chat-hours").innerHTML=nextHours;
 };
 
-function hoursOfOperation() {
 
-// // Assign current time to a variable
-const now = new Date();
-// const now = new Date("2018-04-17T17:00:00");
-contactStatus = "unknown";
-
-console.log(now);
-
-if (
-    (now.getDay()>0 && now.getDay()<6) &&
-    (now.getUTCHours()>=12 && now.getUTCHours()<21)
-    ){
-    chatOpen();
-} else {
-    chatClosed();
-}
-
-console.log(chatStatus);
-
-if (
-    (now.getDay()>0 && now.getDay()<5) &&
-    (now.getUTCHours()>=11 && now.getUTCHours()<23)){
-    phoneOpen();
-} else if(now.getDay()===0 && now.getUTCHours()>=18 && now.getUTCHours()<23){
-    phoneOpen();
-} else if(
-    (now.getDay()===5) &&
-    (now.getUTCHours()>=11 && now.getUTCHours()<22)){
-        phoneOpen();
-} else {
-        phoneClosed();
-}
-
-console.log(phoneStatus);
-
-}
 
 
