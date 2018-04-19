b5d29d24b264d2ef81a5c66ddab54070706cac58
linmmill <35968916+linmmill@users.noreply.github.com>
2018-04-19 12:23:17 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
 var DateTime = luxon.DateTime;
 var DateTimeEST = DateTime.fromObject({zone: 'America/New_York'});
 // Test Closed Text
-// var DateTimeEST =  DateTime.local(2018, 4, 20, 19, 00);
+// var DateTimeEST =  DateTime.local(2018, 4, 21, 19, 00);
 var dayEST = DateTimeEST.weekday;
 var hourEST = DateTimeEST.hour;
 console.log("The ET day is " + dayEST + " and the ET hour is " + hourEST);
@@ -43,7 +43,7 @@ function phoneClosed(){
     document.getElementById("phoneStatus").innerHTML=phoneStatus;
     $( "#phone-btn" ).addClass( "btn-closed" );
     $( "#phoneStatus" ).addClass( "btn-status-closed" );
-    var nextDay = dayEST+1;
+    var nextDay = (dayEST > 5) ? 0 : dayEST+1;
     var hoursOnDay = phoneHours[nextDay];
     nextHours = "Tomorrow's Hours: " + days[nextDay] + " " + hoursOnDay;
     console.log("Next day is " + nextDay);
@@ -81,7 +81,7 @@ function chatClosed(){
     document.getElementById("chatStatus").innerHTML=chatStatus;
     $( "#chat-btn" ).addClass( "btn-closed" );
     $( "#chatStatus" ).addClass( "btn-status-closed" );
-    var nextDay = dayEST+1;
+    var nextDay = (dayEST > 5) ? 0 : dayEST+1;
     var hoursOnDay = chatHours[nextDay];
     nextHours = "Tomorrow's Hours: " + days[nextDay] + " " + hoursOnDay;
     console.log("Next day is " + nextDay);
